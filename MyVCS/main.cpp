171d975fea504634d9d77d55f577c98788692739
Ahmed Hany Saber Abo Zed Ahmed <130825620+AhmedHanySaber@users.noreply.github.com>
2024-07-22 12:18:30 +0300

    Initial Commit

---
+++ MyVCS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MyVCS
+//
+//  Created by Mac on 22/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
